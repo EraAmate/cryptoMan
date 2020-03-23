8349c3bbf6fe283fdeccfc73558443ab1011f616
EraAmate <valeriiaera@gmail.com>
2020-03-23 15:43:32 +0100

    Set passwords

--- app.js
+++ app.js
@@ -24,7 +24,26 @@ function get() {
 
 function set() {
   console.log("Called SET", key, value);
+
+  // READ dbJSON + update by key
+  const content = { content };
+
+  try {
+    let passwordsJSON = fs.readFileSync("db.json", "utf8");
+    let passwords = JSON.parse(passwordsJSON);
+    // UPDATE
+    passwords[key] = value;
+
+    fs.writeFileSync("db.json", JSON.stringify(passwords, null, 2)); // db.json formatierung
+
+    //     passwordsJSON = fs.writeFileSync("db.json", content);
+    //     passwords = JSON.stringify(passwordsJSON);
+    //     console.log(key, passwords[value]);
+  } catch (error) {
+    console.error(error);
+  }
 }
+
 if (command === "get") {
   get();
 } else if (command === "set") {
