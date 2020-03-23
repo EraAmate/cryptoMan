1d3e7466ecf4f36f43f3abfa6433d2c050f28a4b
EraAmate <valeriiaera@gmail.com>
2020-03-23 14:41:23 +0100

    Get passwords by key

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
+const fs = require("fs");
 const [command, key, value] = process.argv.slice(2);
+
+// fs.readFile("db.json","utf8", (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+// });
 
 function get() {
   console.log("Called GET", key);
   // Read and log db.json
-  console.log("db");
+
+  try {
+    const passwordsJSON = fs.readFileSync("db.json", "utf8");
+    const passwords = JSON.parse(passwordsJSON);
+    console.log(key, passwords[key]);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function set() {
