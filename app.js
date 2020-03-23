7bb48db6a951f7422c4c9b6c6507eb6370f90f79
EraAmate <valeriiaera@gmail.com>
2020-03-23 17:04:54 +0100

    Outsource the read/write functions and create module

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-const fs = require("fs");
+const { readPasswords, writePasswords } = require("./lib/passwords");
 const [command, key, value] = process.argv.slice(2);
-
-// fs.readFile("db.json","utf8", (err, data) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   console.log(data);
-// });
 
 function get() {
   console.log("Called GET", key);
   // Read and log db.json
 
   try {
-    const passwordsJSON = fs.readFileSync("db.json", "utf8");
-    const passwords = JSON.parse(passwordsJSON);
+    const passwords = readPasswords();
     console.log(key, passwords[key]);
   } catch (error) {
     console.error(error);
@@ -29,12 +20,12 @@ function set() {
   const content = { content };
 
   try {
-    let passwordsJSON = fs.readFileSync("db.json", "utf8");
-    let passwords = JSON.parse(passwordsJSON);
+    const passwords = readPasswords();
+
     // UPDATE
     passwords[key] = value;
     //WRITE DB.JSON
-    fs.writeFileSync("db.json", JSON.stringify(passwords, null, 2)); // db.json formatierung
+    writePasswords(passwords);
   } catch (error) {
     console.error(error);
   }
@@ -43,10 +34,10 @@ function set() {
 function unset() {
   console.log("Called SET", key, value);
   try {
-    let passwordsJSON = fs.readFileSync("db.json", "utf8");
-    let passwords = JSON.parse(passwordsJSON);
+    const passwords = readPasswords();
+
     delete passwords[key];
-    fs.writeFileSync("db.json", JSON.stringify(passwords, null, 2)); // db.json formatierung
+    writePasswords(passwords);
   } catch (error) {
     console.error(error);
   }
