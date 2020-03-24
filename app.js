c9b9cbd9ddd79169b6b9f30eb23fba31f7afb00e
EraAmate <valeriiaera@gmail.com>
2020-03-24 16:30:32 +0100

    Ask for master password

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ const [command, key] = process.argv.slice(2);
 const { askForPassword } = require("./lib/questions");
 const { get, set, unset } = require("./lib/commands");
 
+const { askForMasterPassword } = require("./lib/questions");
+const { readMasterPassword } = require("./lib/passwords");
+
 async function run() {
+  const answeredMasterPassword = await askForMasterPassword();
+  const masterPassword = readMasterPassword();
+
+  if (answeredMasterPassword !== masterPassword) {
+    console.error("Fuck off!");
+    return;
+  }
+
   if (command === "get") {
     get(key);
   } else if (command === "set") {
